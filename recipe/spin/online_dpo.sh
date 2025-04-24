6c7f4960ef747422330c36242f01591169300468
hebiao064 <hebiaobuaa@gmail.com>
2025-04-24 05:25:12 +0000

    update to qwen2

--- recipe/spin/online_dpo.sh
+++ recipe/spin/online_dpo.sh
@@ -17,7 +17,7 @@ export WANDB_API_KEY=25c95cfb8dfe322ae6d944a369d2ae63b65d9ece
 
 
 # MODEL_NAME="Qwen/Qwen2-7B-Instruct"
-MODEL_NAME="/shared/public/elr-models/Qwen/Qwen2.5-7B-Instruct/52e20a6f5f475e5c8f6a8ebda4ae5fa6b1ea22ac"
+MODEL_NAME="/shared/public/sharing/bhe/qwen/Qwen2-7B-Instruct/snapshots/f2826a00ceef68f0f2b946d945ecc0477ce4450c"
 BASE_MODEL_PATH=$MODEL_NAME
 REWARD_MODEL_PATH="/shared/public/sharing/bhe/qwen/Qwen2-0.5B-Reward/snapshots/80fb188cabd3e854c2fb983f23a153c0e58a69e0"
 
